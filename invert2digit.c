761f2ea2d5bb3bd9ba7d2a2132c1c942aa3afc69
Luca Pignatelli <lucapignatelli89@gmail.com>
2016-01-21 12:47:55 +0100

    Create invert2digit.c

---
+++ invert2digit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+    int i, j;
+    
+        printf("Enter a two-digit number:");
+        scanf("%1d%1d", &i, &j);
+        printf("The reversal is: 1d%1d\n", j, i);
+        
+    return 0; 
+}    
